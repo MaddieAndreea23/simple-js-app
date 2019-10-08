424e7a6dff2119d23588e7ec5439b8775867560c
Maddie <mad_dy.andrea@hotmail.com>
2019-10-08 14:53:40 +0200

    Respository

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-alert('Hello world');
+var repository = [
+  { name : 'Ninetales',
+    height : 1.1,
+    weight : 19.9,
+    abilities : [ 'flash-fire' , 'drought' ]
+  },
 
-var favouriteFood = 'Pasta';
-document.write(favouriteFood);
+  { name : 'Flareon',
+    height : 0.9,
+    weight : 25,
+    abilities : [ 'flash-fire' , 'guts' ]
+  },
+
+  { name : 'Umbreon',
+    height : 1,
+    weight : 27,
+    abilities : [ 'synchronize' , 'inner-focus' ]
+  }
+]
