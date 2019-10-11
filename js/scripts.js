8c138960b672b41f6b595c7500466d4e29768872
Maddie <mad_dy.andrea@hotmail.com>
2019-10-11 15:13:32 +0200

    for, if, else

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,30 @@
 var repository = [
   {
     name: "Ninetales",
-    height: 1.1,
+    height: 5,
     weight: 19.9,
     type: "fire",
     abilities: ["flash-fire", "drought"]
   },
-
   {
     name: "Umbreon",
-    height: 1,
+    height: 3,
     weight: 27,
     type: "dark",
     abilities: ["synchronize", "inner-focus"]
   },
-
   {
     name: "Flareon",
-    height: 0.9,
+    height: 2,
     weight: 25,
     type: "fire",
     abilities: ["flash-fire", "guts"]
   }
-];
-
-var name = "Ninentales";
-let height = 1.1;
-for (let i = 0; i >= 0 ; i--) {
-  if(i > 1){
-    document.write(name + "(height:1.1)");
+]
+for (var i = 0; i < repository.lenght; i++) {
+  if(repository[i].height > 1 && repository[i].height <= 3) {
+    document.write(repository[i].name + "(" + repository[i].height + ")");
   }else{
-    document.write(name + "(height:1.1)" + " - Wow, that's so big!");
+    document.write(repository[i].name + "(" + repository[i].height + ")" + " - Wow, that's so big!");
   }
 }
