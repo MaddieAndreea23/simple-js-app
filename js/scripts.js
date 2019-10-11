f46e69d45aa8616a561127eacb7925cfa3aa5cac
Maddie <mad_dy.andrea@hotmail.com>
2019-10-11 14:17:58 +0200

    Loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
 var repository = [
-  { name : 'Ninetales',
-    height : 1.1,
-    weight : 19.9,
-    abilities : [ 'flash-fire' , 'drought' ]
+  {
+    name: "Ninetales",
+    height: 1.1,
+    weight: 19.9,
+    type: "fire",
+    abilities: ["flash-fire", "drought"]
   },
 
-  { name : 'Flareon',
-    height : 0.9,
-    weight : 25,
-    abilities : [ 'flash-fire' , 'guts' ]
+  {
+    name: "Umbreon",
+    height: 1,
+    weight: 27,
+    type: "dark",
+    abilities: ["synchronize", "inner-focus"]
   },
 
-  { name : 'Umbreon',
-    height : 1,
-    weight : 27,
-    abilities : [ 'synchronize' , 'inner-focus' ]
+  {
+    name: "Flareon",
+    height: 0.9,
+    weight: 25,
+    type: "fire",
+    abilities: ["flash-fire", "guts"]
   }
-]
+];
+
+var name = "Ninentales";
+let height = 1.1;
+for (let i = 0; i >= 0 ; i--) {
+  if(i > 1){
+    document.write(name + "(height:1.1)");
+  }else{
+    document.write(name + "(height:1.1)" + " - Wow, that's so big!");
+  }
+}
